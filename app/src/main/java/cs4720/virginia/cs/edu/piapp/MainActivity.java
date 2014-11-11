1be10113e3c4dbcbca950b596e5093795930d6a8
ems4pc@virginia.edu <youremail@ccri.com>
2014-11-11 13:34:10 -0500

    added helper functions and fixed db work

--- app/src/main/java/cs4720/virginia/cs/edu/piapp/MainActivity.java
+++ app/src/main/java/cs4720/virginia/cs/edu/piapp/MainActivity.java
@@ -2,14 +2,11 @@ package cs4720.virginia.cs.edu.piapp;
 
 import android.app.Activity;
 import android.app.AlertDialog;
-import android.content.ContentValues;
-import android.content.Context;
 import android.content.DialogInterface;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteOpenHelper;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.provider.BaseColumns;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -19,13 +16,27 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.hmkcode.http.HttpHandler;
+
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.message.BasicHeader;
+import org.apache.http.protocol.HTTP;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+
+import static cs4720.virginia.cs.edu.piapp.FeedReaderContract.FeedEntry.COLUMN_NAME_JSON;
+import static cs4720.virginia.cs.edu.piapp.FeedReaderContract.FeedEntry.COLUMN_NAME_TITLE;
+import static cs4720.virginia.cs.edu.piapp.FeedReaderContract.FeedEntry.TABLE_NAME;
 
 
 public class MainActivity extends Activity  {
     Activity activity;
-
 
     /** Called when the activity is first created. */
     @Override
@@ -34,8 +45,143 @@ public class MainActivity extends Activity  {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         this.activity = this;
+        setButtonListeners();
+        populateList();
+    }
+
+    /*
+        Should be used in Kristen's Add Light Show
+    private void addToDB(String json, String name) {
+        ContentValues values = new ContentValues();
+        values.put(COLUMN_NAME_JSON, json);
+        values.put(COLUMN_NAME_TITLE, name);
+
+        db.insert(TABLE_NAME, null,values);
+    }
+    */
+
+    private void populateList() {
+        FeedReaderDbHelper mDbHelper = new FeedReaderDbHelper(getBaseContext());
+        SQLiteDatabase db = mDbHelper.getWritableDatabase();
+        String[] projection = {COLUMN_NAME_TITLE, COLUMN_NAME_JSON};
+        String sortOrder =COLUMN_NAME_TITLE + " DESC";
+        Cursor c = db.query(TABLE_NAME, projection, null, null, null, null, sortOrder);
+        final ListView listView = (ListView) findViewById(R.id.list);
+        int numOfRows = c.getCount();
+        String[] list_values = getNames(numOfRows, c);
+        c.close();
+        db.close();
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, android.R.id.text1, list_values);
+
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                int itemPosition     = position;
+                String  itemValue    = (String) listView.getItemAtPosition(position);
+                Toast.makeText(getApplicationContext(), "Now Showing: " + itemValue, Toast.LENGTH_LONG)
+                        .show();
+                FeedReaderDbHelper mDbHelper = new FeedReaderDbHelper(getBaseContext());
+                SQLiteDatabase db = mDbHelper.getWritableDatabase();
+
+                String[] projection = {COLUMN_NAME_TITLE, COLUMN_NAME_JSON};
+                String sortOrder =COLUMN_NAME_TITLE + " DESC";
+                Cursor c = db.query(TABLE_NAME, projection, null, null, null, null, sortOrder);
+                JSONObject json;
+                if (c.getCount() > 0) {
+                    c.moveToPosition(position);
+                    json = parseJson(c.getString(1));
+                } else {
+                    json = new JSONObject();
+                    try {
+                        json.put("test", "one");
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                makePostRequest(json);
+
+                c.close();
+                db.close();
+            }
+        });
+    }
+
+    public JSONObject parseJson(String json) {
+        return new JSONObject();
+    }
+
+    public void makePostRequest(JSONObject json) {
+        new HttpHandler() {
+            @Override
+            public HttpUriRequest getHttpRequestMethod() {
+                TextView tx = (TextView) findViewById(R.id.textView);
+                String url = tx.getText().toString().split("Current Ip Address: ")[1];
+                HttpPost p = new HttpPost("http://" + url + "/rpi");
+                p.addHeader("Content-type", "application/json");
+
+                try {
+
+                    JSONObject lightId = new JSONObject();
+                    lightId.put("lightId", 1);
+                    lightId.put("red", Color.red(255));
+                    lightId.put("green", Color.green(0));
+                    lightId.put("blue", Color.blue(0));
+                    lightId.put("intensity", 30);
+                    JSONArray lights = new JSONArray();
+                    lights.put(lightId);
+                    JSONObject main = new JSONObject();
+                    main.put("lights", lights);
+                    main.put("propagate", true);
+                    StringEntity se = new StringEntity(main.toString());
+                    se.setContentType(new BasicHeader(HTTP.CONTENT_TYPE,"application/json"));
+                    p.setEntity(se);
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+                return p;
+            }
+
+            @Override
+            public void onResponse(String s) {
+
+            }
+        }.execute();
+
+    }
+
+    private String[] getNames(int numOfRows, Cursor c) {
+        ArrayList<String> names = new ArrayList<String>();
+
+        if (numOfRows == 0) {
+            String[] fake_data = {"Light Show 1", "Light Show 2"};
+            return fake_data;
+        } else {
+            for (int i = 0; i < numOfRows; i++) {
+                c.moveToPosition(0);
+                names.add(c.getString(0));
+            }
+
+            return names.toArray(new String[names.size()]);
+        }
+    }
+//      Possibly use something like this when deleting a light show
+//      will clear database if necessary
+//
+//    private void clearDB() {
+//        db.delete(TABLE_NAME, null, null);
+//    }
+
+    private void setButtonListeners() {
         final Button updateIpButton = (Button) findViewById(R.id.popupbutton);
-        JSONObject json = new JSONObject();
+        final Button addNewLightShow = (Button) findViewById(R.id.button_add_new_light);
 
         updateIpButton.setOnClickListener(new View.OnClickListener() {
 
@@ -45,80 +191,18 @@ public class MainActivity extends Activity  {
             }
         });
 
-
-        try {
-            json.put("test", true);
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-
-        FeedReaderDbHelper mDbHelper = new FeedReaderDbHelper(getBaseContext());
-        SQLiteDatabase db = mDbHelper.getWritableDatabase();
-
-// Create a new map of values, where column names are the keys
-        ContentValues values = new ContentValues();
-        values.put(FeedReaderContract.FeedEntry.COLUMN_NAME_JSON, "id");
-        values.put(FeedReaderContract.FeedEntry.COLUMN_NAME_TITLE, "Name of Light Show");
-
-// Insert the new row, returning the primary key value of the new row
-
-        long newRowId = db.insert(FeedReaderContract.FeedEntry.TABLE_NAME, null,values);
-
-        String[] projection = {
-                FeedReaderContract.FeedEntry.COLUMN_NAME_TITLE,
-                FeedReaderContract.FeedEntry.COLUMN_NAME_JSON,
-        };
-
-// How you want the results sorted in the resulting Cursor
-        String sortOrder =
-                FeedReaderContract.FeedEntry.COLUMN_NAME_TITLE + " DESC";
-
-        Cursor c = db.query(
-                FeedReaderContract.FeedEntry.TABLE_NAME,  // The table to query
-                projection,                               // The columns to return
-                null,                                // The columns for the WHERE clause
-                null,                            // The values for the WHERE clause
-                null,                                     // don't group the rows
-                null,                                     // don't filter by row groups
-                sortOrder                                 // The sort order
-        );
-
-        final ListView listView = (ListView) findViewById(R.id.list);
-
-        c.moveToFirst();
-
-        String name = c.getString(
-                c.getColumnIndexOrThrow(FeedReaderContract.FeedEntry.COLUMN_NAME_TITLE)
-        );
-
-
-        String[] list_values = new String[] {
-                "Name of Light Show",
-                "Name 2 of Light Show",
-                name
-        };
-
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1, android.R.id.text1, list_values);
-
-        listView.setAdapter(adapter);
-
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        addNewLightShow.setOnClickListener(new View.OnClickListener() {
 
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                int itemPosition     = position;
-                String  itemValue    = (String) listView.getItemAtPosition(position);
-//                Toast.makeText(getApplicationContext(),
-//                        "Position :"+itemPosition+"  ListItem : " +itemValue , Toast.LENGTH_LONG)
-//                        .show();
-                Toast.makeText(getApplicationContext(), "Now Playing: " + itemValue, Toast.LENGTH_LONG)
-                        .show();
+            public void onClick(View v) {
+                addLightShow(v);
             }
-
         });
     }
 
+    private void addLightShow(View v) {
+        //add connection to kristen's add light show activity
+    }
 
 
     private void openIpDialog(View view) {
@@ -144,50 +228,5 @@ public class MainActivity extends Activity  {
         AlertDialog alertDialog = alertDialogBuilder.create();
 
         alertDialog.show();
-    }
-
-    public final class FeedReaderContract {
-        // To prevent someone from accidentally instantiating the contract class,
-        // give it an empty constructor.
-        public FeedReaderContract() {}
-
-        /* Inner class that defines the table contents */
-        public abstract class FeedEntry implements BaseColumns {
-            public static final String TABLE_NAME = "entry";
-            public static final String COLUMN_NAME_JSON = "entryid";
-            public static final String COLUMN_NAME_TITLE = "title";
-        }
-    }
-
-    private static final String TEXT_TYPE = " TEXT";
-    private static final String COMMA_SEP = ",";
-    private static final String SQL_CREATE_ENTRIES =
-            "CREATE TABLE " + FeedReaderContract.FeedEntry.TABLE_NAME + " (" +
-                    FeedReaderContract.FeedEntry._ID + " INTEGER PRIMARY KEY," +
-                    FeedReaderContract.FeedEntry.COLUMN_NAME_JSON + TEXT_TYPE + COMMA_SEP +
-                    FeedReaderContract.FeedEntry.COLUMN_NAME_TITLE + TEXT_TYPE + " );";
-
-    private static final String SQL_DELETE_ENTRIES =
-            "DROP TABLE IF EXISTS " + FeedReaderContract.FeedEntry.TABLE_NAME;
-    public class FeedReaderDbHelper extends SQLiteOpenHelper {
-        // If you change the database schema, you must increment the database version.
-        public static final int DATABASE_VERSION = 1;
-        public static final String DATABASE_NAME = "FeedReader.db";
-
-        public FeedReaderDbHelper(Context context) {
-            super(context, DATABASE_NAME, null, DATABASE_VERSION);
-        }
-        public void onCreate(SQLiteDatabase db) {
-            db.execSQL(SQL_CREATE_ENTRIES);
-        }
-        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-            // This database is only a cache for online data, so its upgrade policy is
-            // to simply to discard the data and start over
-            db.execSQL(SQL_DELETE_ENTRIES);
-            onCreate(db);
-        }
-        public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-            onUpgrade(db, oldVersion, newVersion);
-        }
     }
 }
