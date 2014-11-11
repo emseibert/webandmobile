5fee418f15f95ab9c26b69c249698f4d41d49598
ems4pc@virginia.edu <youremail@ccri.com>
2014-11-11 01:54:12 -0500

    new_home_page

--- app/src/main/java/cs4720/virginia/cs/edu/piapp/MainActivity.java
+++ app/src/main/java/cs4720/virginia/cs/edu/piapp/MainActivity.java
@@ -1,133 +1,193 @@
 package cs4720.virginia.cs.edu.piapp;
 
 import android.app.Activity;
-import android.graphics.Color;
-import android.graphics.drawable.*;
+import android.app.AlertDialog;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
+import android.provider.BaseColumns;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.util.List;
-import java.util.ArrayList;
-import org.apache.http.*;
-import org.apache.http.client.*;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.message.BasicHeader;
-import org.apache.http.message.BasicNameValuePair;
-//import org.apache.http.impl.client.DefaultHttpClient;
-//import org.apache.http.client.methods.HttpPost;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpUriRequest;
-import org.apache.http.protocol.HTTP;
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import com.hmkcode.http.HttpHandler;
-import android.widget.TextView;
-import android.widget.Toast;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
 
-public class MainActivity extends Activity implements ColorPicker.OnColorChangedListener {
-    ColorPicker p;
-    int c;
+public class MainActivity extends Activity  {
+    Activity activity;
+
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         this.activity = this;
-        final Button b = (Button) findViewById(R.id.button);
-        p = new ColorPicker(activity, MainActivity.this, Color.WHITE);
-        if (isConnected()) {
-            TextView tx = (TextView) findViewById(R.id.textView2);
-            tx.setText("You are connected");
-            tx.setBackgroundColor(Color.GREEN);
-        }
-        else {
-            TextView tx = (TextView) findViewById(R.id.textView2);
-            tx.setText("You are not connected");
-            tx.setBackgroundColor(Color.RED);
-        }
-        b.setOnClickListener(new View.OnClickListener() {
+        final Button updateIpButton = (Button) findViewById(R.id.popupbutton);
+        JSONObject json = new JSONObject();
+
+        updateIpButton.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View v) {
-                p.show();
-                ColorDrawable drawable = (ColorDrawable) b.getBackground();
-
+                openIpDialog(v);
             }
+        });
+
+
+        try {
+            json.put("test", true);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        FeedReaderDbHelper mDbHelper = new FeedReaderDbHelper(getBaseContext());
+        SQLiteDatabase db = mDbHelper.getWritableDatabase();
+
+// Create a new map of values, where column names are the keys
+        ContentValues values = new ContentValues();
+        values.put(FeedReaderContract.FeedEntry.COLUMN_NAME_JSON, "id");
+        values.put(FeedReaderContract.FeedEntry.COLUMN_NAME_TITLE, "Name of Light Show");
+
+// Insert the new row, returning the primary key value of the new row
+
+        long newRowId = db.insert(FeedReaderContract.FeedEntry.TABLE_NAME, null,values);
+
+        String[] projection = {
+                FeedReaderContract.FeedEntry.COLUMN_NAME_TITLE,
+                FeedReaderContract.FeedEntry.COLUMN_NAME_JSON,
+        };
+
+// How you want the results sorted in the resulting Cursor
+        String sortOrder =
+                FeedReaderContract.FeedEntry.COLUMN_NAME_TITLE + " DESC";
+
+        Cursor c = db.query(
+                FeedReaderContract.FeedEntry.TABLE_NAME,  // The table to query
+                projection,                               // The columns to return
+                null,                                // The columns for the WHERE clause
+                null,                            // The values for the WHERE clause
+                null,                                     // don't group the rows
+                null,                                     // don't filter by row groups
+                sortOrder                                 // The sort order
+        );
+
+        final ListView listView = (ListView) findViewById(R.id.list);
+
+        c.moveToFirst();
+
+        String name = c.getString(
+                c.getColumnIndexOrThrow(FeedReaderContract.FeedEntry.COLUMN_NAME_TITLE)
+        );
+
+
+        String[] list_values = new String[] {
+                "Name of Light Show",
+                "Name 2 of Light Show",
+                name
+        };
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, android.R.id.text1, list_values);
+
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                int itemPosition     = position;
+                String  itemValue    = (String) listView.getItemAtPosition(position);
+//                Toast.makeText(getApplicationContext(),
+//                        "Position :"+itemPosition+"  ListItem : " +itemValue , Toast.LENGTH_LONG)
+//                        .show();
+                Toast.makeText(getApplicationContext(), "Now Playing: " + itemValue, Toast.LENGTH_LONG)
+                        .show();
+            }
+
         });
     }
 
-    @Override
-    public void colorChanged(int color) {
-        MainActivity.this.findViewById(android.R.id.content).setBackgroundColor(color);
-        c = color;
-        postData2();
-    }
-
-    Activity activity;
-
-    public void getColor(View v) {
-        new ColorPicker(activity, MainActivity.this, Color.WHITE).show();
-    }
-
-    public boolean isConnected(){
-        ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Activity.CONNECTIVITY_SERVICE);
-        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
-        if (networkInfo != null && networkInfo.isConnected())
-            return true;
-        else
-            return false;
-    }
-
-    public void postData2() {
-        new HttpHandler() {
-            @Override
-            public HttpUriRequest getHttpRequestMethod() {
-                TextView tx = (TextView) findViewById(R.id.editText);
-                String url = tx.getText().toString();
-
-                HttpPost p = new HttpPost("http://" + url + "/rpi");
-                p.addHeader("Content-type", "application/json");
 
 
-                try {
+    private void openIpDialog(View view) {
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(MainActivity.this);
+        final EditText input = new EditText(this);
+        alertDialogBuilder.setView(input);
+        alertDialogBuilder.setTitle("Update IP Address");
 
-                    JSONObject lightId = new JSONObject();
-                    lightId.put("lightId", 1);
-                    lightId.put("red", Color.red(c));
-                    lightId.put("green", Color.green(c));
-                    lightId.put("blue", Color.blue(c));
-                    lightId.put("intensity", 30);
-                    JSONArray lights = new JSONArray();
-                    lights.put(lightId);
-                    JSONObject main = new JSONObject();
-                    main.put("lights", lights);
-                    main.put("propagate", true);
-                    StringEntity se = new StringEntity(main.toString());
-                    se.setContentType(new BasicHeader(HTTP.CONTENT_TYPE,"application/json"));
-                    p.setEntity(se);
-                } catch (UnsupportedEncodingException e) {
-                    e.printStackTrace();
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-
-                return p;
+        alertDialogBuilder.setPositiveButton("Update", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                String value = input.getText().toString().trim();
+                TextView tx = (TextView) findViewById(R.id.textView);
+                tx.setText("Current IP Address: " + value);
+                Toast.makeText(getApplicationContext(), value, Toast.LENGTH_SHORT).show();
             }
-            @Override
-            public void onResponse(String result) {
-                Toast.makeText(getBaseContext(), "Received!", Toast.LENGTH_LONG).show();
+        });
+
+        alertDialogBuilder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                dialog.cancel();
             }
+        });
+        AlertDialog alertDialog = alertDialogBuilder.create();
 
-        }.execute();
+        alertDialog.show();
+    }
 
+    public final class FeedReaderContract {
+        // To prevent someone from accidentally instantiating the contract class,
+        // give it an empty constructor.
+        public FeedReaderContract() {}
+
+        /* Inner class that defines the table contents */
+        public abstract class FeedEntry implements BaseColumns {
+            public static final String TABLE_NAME = "entry";
+            public static final String COLUMN_NAME_JSON = "entryid";
+            public static final String COLUMN_NAME_TITLE = "title";
+        }
+    }
+
+    private static final String TEXT_TYPE = " TEXT";
+    private static final String COMMA_SEP = ",";
+    private static final String SQL_CREATE_ENTRIES =
+            "CREATE TABLE " + FeedReaderContract.FeedEntry.TABLE_NAME + " (" +
+                    FeedReaderContract.FeedEntry._ID + " INTEGER PRIMARY KEY," +
+                    FeedReaderContract.FeedEntry.COLUMN_NAME_JSON + TEXT_TYPE + COMMA_SEP +
+                    FeedReaderContract.FeedEntry.COLUMN_NAME_TITLE + TEXT_TYPE + " );";
+
+    private static final String SQL_DELETE_ENTRIES =
+            "DROP TABLE IF EXISTS " + FeedReaderContract.FeedEntry.TABLE_NAME;
+    public class FeedReaderDbHelper extends SQLiteOpenHelper {
+        // If you change the database schema, you must increment the database version.
+        public static final int DATABASE_VERSION = 1;
+        public static final String DATABASE_NAME = "FeedReader.db";
+
+        public FeedReaderDbHelper(Context context) {
+            super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        }
+        public void onCreate(SQLiteDatabase db) {
+            db.execSQL(SQL_CREATE_ENTRIES);
+        }
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            // This database is only a cache for online data, so its upgrade policy is
+            // to simply to discard the data and start over
+            db.execSQL(SQL_DELETE_ENTRIES);
+            onCreate(db);
+        }
+        public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            onUpgrade(db, oldVersion, newVersion);
+        }
     }
 }
